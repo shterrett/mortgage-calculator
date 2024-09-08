84585788e5eabc9474c0130a05a87752f134206d
Stuart Terrett <shterrett@gmail.com>
2024-09-08 09:18:58 -0400

    wire it up

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,27 @@
+{-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 
 module Main (main) where
 
+import Data.Text.IO qualified as Text
+import Format (tableFormat)
+import MortgageCalculator (calculate, Mortgage(..), Payment(..), dollars, apr)
 import Options (Options(..), unwrapRecord)
+import Parser (parsePayments)
 
 main :: IO ()
 main = do
-  o@Options{..} <- unwrapRecord "mortgage-calculator"
-  putStrLn $ show o
-  pure ()
+  Options{..} <- unwrapRecord "mortgage-calculator"
+  paymentFile <- Text.readFile payments
+  case parsePayments paymentFile of
+    Left e -> error e
+    Right ps -> do
+      let
+        end = maybe (maximum $ (\Payment{..} -> date) <$> ps) id endDate
+        mortgage = Mortgage { initialPrincipal = dollars initialPrincipal
+                            , interestRate = apr interestRate
+                            , extraInterest = extraInterest
+                            }
+        events = calculate startDate end mortgage ps showAll
+      putStrLn $ tableFormat events
